3b85b166a506bdca5c6aed0fd798c3a4ca2bf0a5
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-09-11 18:50:31 +0700

    Setup folder, design page not found , import tailwind, use shadcn, axios, sonner, lucide , react router, patterncraft,.. config project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { Toaster } from "sonner";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  
   return (
-    <></>
-  )
+    <>
+    <Toaster/>
+      <BrowserRouter>
+        <Routes>
+          {/* Route cho trang chủ */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* Route fallback cho các đường dẫn không tồn tại */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
