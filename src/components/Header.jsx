5a52cd4169387840a2f23b9ff99e5f4fd303a1f3
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-09-12 19:30:58 +0700

    Code Ui & UX For Home Page: Header, Add Task, Stats and Filter

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {
+  return (
+    <div className="space-y-2 text-center">
+      <h1 className="text-4xl font-bold text-transparent bg-primary bg-clip-text inline-block leading-[1.15] pb-1">
+        Digital X Automation - ndyudev 
+      </h1>
+
+      <p className="text-muted-foreground">
+        "Every great app started with one line of code. Keep typing, keep
+        building, keep growing." 💻
+      </p>
+    </div>
+  );
+}
+
+export default Header
--- src/components/ui/StatsAndFilters.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function StatsAndFilters() {
-  return (
-    <div>StatsAndFilters</div>
-  )
-}
-
-export default StatsAndFilters
