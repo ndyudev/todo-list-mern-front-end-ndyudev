532eb2c1082c12e936338bd1476d432965121a1a
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-09-29 19:02:13 +0700

    Crud Task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import TaskEmtyState from "./TaskEmtyState";
 import TaskCard from "./TaskCard";
 
-const TaskList = ({ filteredTasks, filter }) => {
-
+const TaskList = ({ filteredTasks, filter, handleTaskChanged }) => {
   if (!filteredTasks || filteredTasks.length === 0) {
     return <TaskEmtyState filter={filter} />;
   }
@@ -11,10 +10,15 @@ const TaskList = ({ filteredTasks, filter }) => {
   return (
     <div className="space-y-3">
       {filteredTasks.map((task, index) => (
-        <TaskCard key={task._id ?? index} task={task} index={index} />
+        <TaskCard
+          key={task._id ?? index}
+          task={task}
+          index={index}
+          handleTaskChanged={handleTaskChanged}
+        />
       ))}
     </div>
   );
-}
+};
 
 export default TaskList;
--- src/lib/data.jsx
+++
@@ -1,5 +0,0 @@
-export const FilterType = {
-    all: "tất cả",
-    active: "đang làm",
-    completed: "hoàn thành"
-};
